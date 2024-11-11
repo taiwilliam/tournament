69fdefaabe8a2a1526239aea6d01c870277c6c5b
William Tai <44943338+taiwilliam@users.noreply.github.com>
2024-11-11 18:33:29 +0800

    分組函數

--- src/data.js
+++ src/data.js
@@ -54,6 +54,7 @@ export const participants_16 = [
       tournament_id: 0,
       name: '昀臻'
   },
+  null,
   {
       id: 12,
       tournament_id: 0,
@@ -74,7 +75,6 @@ export const participants_16 = [
       tournament_id: 0,
       name: '韡翔'
   }
-
 ]
 
 export const participants_8 = [
@@ -137,3 +137,82 @@ export const participants_4 = [
       name: '亨利'
   }
 ]
+
+
+export const group_data = [
+    {
+        id: 1,
+        points: 800,
+        name: '威廉'
+    },
+    {
+        id: 2,
+        points: 700,
+        name: '凱恩'
+    },
+    {
+        id: 3,
+        points: 1200,
+        name: '約翰'
+    },
+    {
+        id: 4,
+        points: 2200,
+        name: '亨利'
+    },
+    {
+        id: 5,
+        points: 1000,
+        name: '西卡'
+    },
+    {
+        id: 6,
+        points: 550,
+        name: '特斯拉'
+    },
+    {
+        id: 7,
+        points: 550,
+        name: '愛迪生'
+    },
+    {
+        id: 8,
+        points: 430,
+        name: '威爾'
+    },
+    {
+        id: 9,
+        points: 1800,
+        name: '佳萱'
+    },
+    {
+        id: 10,
+        points: 2800,
+        name: '許武'
+    },
+    {
+        id: 11,
+        points: 2700,
+        name: '昀臻'
+    },
+    {
+        id: 12,
+        points: 1300,
+        name: '建麟'
+    },
+    {
+        id: 13,
+        points: 500,
+        name: '璇'
+    },
+    {
+        id: 14,
+        points: 1380,
+        name: '虹妤'
+    },
+    {
+        id: 15,
+        points: 380,
+        name: '韡翔'
+    }
+  ]
